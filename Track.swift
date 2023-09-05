f3e2575ebf0926994d0b970bc638fc67640e894d
ADM <90643958+adm410@users.noreply.github.com>
2023-09-05 17:24:11 +0300

    Add files via upload

---
+++ Track.swift
@@ -0,0 +1,207 @@
+//
+//  Track.swift
+//  F1 App
+//
+//  Created by Aditya on 05/09/2023.
+//
+
+
+var  flag = ""
+var laps = ""
+var length = ""
+var distance = ""
+
+public func trackData(_ track: String, _ name: String) {
+    if track == "Bahrain International Circuit" {
+        flag = "🇧🇭"
+        laps = "57"
+        length = "5.412"
+        distance = "308.238"
+    }
+    if track == "Jeddah Corniche Circuit" {
+        flag = "🇸🇦"
+        laps = "50"
+        length = "6.174"
+        distance = "308.45"
+    }
+    if track == "Albert Park Grand Prix Circuit" {
+        flag = "🇦🇺"
+        laps = "58"
+        length = "5.278"
+        distance = "306.124"
+    }
+    if track == "Autodromo Enzo e Dino Ferrari" {
+        flag = "🇮🇹"
+        laps = "63"
+        length = "4.909"
+        distance = "309.049"
+    }
+    if track == "Miami International Autodrome" {
+        flag = "🇺🇸"
+        laps = "57"
+        length = "5.412"
+        distance = "308.326"
+    }
+    if track == "Circuit de Barcelona-Catalunya" {
+        flag = "🇪🇸"
+        laps = "66"
+        length = "4.657"
+        distance = "307.236"
+    }
+    if track == "Circuit de Monaco" {
+        flag = "🇲🇨"
+        laps = "78"
+        length = "3.337"
+        distance = "260.286"
+    }
+    if track == "Baku City Circuit" {
+        flag = "🇦🇿"
+        laps = "51"
+        length = "6.003"
+        distance = "306.049"
+    }
+    if track == "Circuit Gilles Villeneuve" {
+        flag = "🇨🇦"
+        laps = "70"
+        length = "4.361"
+        distance = "305.27"
+    }
+    if track == "Silverstone Circuit" {
+        flag = "🇬🇧"
+        laps = "52"
+        length = "5.891"
+        distance = "306.198"
+    }
+    if track == "Red Bull Ring" {
+        flag = "🇦🇹"
+        laps = "71"
+        length = "4.318"
+        distance = "306.452"
+    }
+    if track == "Circuit Paul Ricard" {
+        flag = "🇫🇷"
+        laps = "53"
+        length = "5.842"
+        distance = "309.69"
+    }
+    if track == "Hungaroring" {
+        flag = "🇭🇺"
+        laps = "70"
+        length = "4.381"
+        distance = "306.63"
+    }
+    if track == "Circuit de Spa-Francorchamps" {
+        flag = "🇧🇪"
+        laps = "44"
+        length = "7.004"
+        distance = "308.052"
+    }
+    if track == "Circuit Park Zandvoort" {
+        flag = "🇳🇱"
+        laps = "72"
+        length = "4.259"
+        distance = "306.587"
+    }
+    if track == "Autodromo Nazionale di Monza" {
+        flag = "🇮🇹"
+        laps = "53"
+        length = "5.793"
+        distance = "306.72"
+    }
+    if track == "Marina Bay Street Circuit" {
+        flag = "🇸🇬"
+        laps = "61"
+        length = "5.063"
+        distance = "308.706"
+    }
+    if track == "Suzuka Circuit" {
+        flag = "🇯🇵"
+        laps = "53"
+        length = "5.807"
+        distance = "307.471"
+    }
+    if track == "Circuit of the Americas" {
+        flag = "🇺🇸"
+        laps = "56"
+        length = "5.513"
+        distance = "308.405"
+    }
+    if track == "Autódromo Hermanos Rodríguez" {
+        flag = "🇲🇽"
+        laps = "71"
+        length = "4.304"
+        distance = "305.354"
+    }
+    if track == "Autódromo José Carlos Pace" {
+        flag = "🇧🇷"
+        laps = "71"
+        length = "4.309"
+        distance = "305.879"
+    }
+    if track == "Yas Marina Circuit" {
+        flag = "🇦🇪"
+        laps = "58"
+        length = "5.281"
+        distance = "306.183"
+    }
+    if track == "Las Vegas Strip Street Circuit" {
+        flag = "🇺🇸"
+        laps = "50"
+        length = "6.116"
+        distance = "305.775"
+    }
+    if track == "Shanghai International Circuit" {
+        flag = "🇨🇳"
+        laps = "56"
+        length = "5.451"
+        distance = "305.066"
+    }
+    if track == "Istanbul Park" {
+        flag = "🇹🇷"
+        laps = "58"
+        length = "5.338"
+        distance = "309.396"
+    }
+    if track == "Losail International Circuit" {
+        flag = "🇶🇦"
+        laps = "57"
+        length = "5.38"
+        distance = "306.66"
+    }
+    if track == "Autódromo Internacional do Algarve" {
+        flag = "🇵🇹"
+        laps = "66"
+        length = "4.653"
+        distance = "306.826"
+    }
+    if track == "Nürburgring" {
+        flag = "🇩🇪"
+        laps = "60"
+        length = "5.148"
+        distance = "308.617"
+    }
+    if track == "Sochi Autodrom" {
+        flag = "🇷🇺"
+        laps = "53"
+        length = "5.848"
+        distance = "309.745"
+    }
+    if name == "Sakhir Grand Prix" {
+        flag = "🇧🇭"
+        laps = "67"
+        length = "3.543"
+        distance = "307.995"
+    }
+    if track == "Hockenheimring" {
+        flag = "🇩🇪"
+        laps = "67"
+        length = "4.574"
+        distance = "306.458"
+    }
+    if track == "Autodromo Internazionale del Mugello" {
+        flag = "🇮🇹"
+        laps = "59"
+        length = "5.245"
+        distance = "309.497"
+    }
+}
